dd986f4258a3bdedcbfc82f339ed0c2fbbc97be3
JhaPrajjwal <info.prajjwaljha@gmail.com>
2019-03-09 16:45:47 +0530

    updated codeforces

--- sources/codeforces.py
+++ sources/codeforces.py
@@ -1,20 +1,29 @@
 import requests
-import json
+from datetime import datetime,timedelta
+from dateutil.parser import parse
 
+cur = datetime.now()
 def codeforces():
     output = requests.get("http://codeforces.com/api/contest.list?gym=false").json()['result']
     
-    idx = 0
+    future_contests = []
     for i in range(len(output)):
         
         if output[i]['phase'] != "BEFORE":
             break
 
-        idx += 1
+        temp = {}
+        temp['link'] = "https://codeforces.com/contest/" + str(output[i]['id'])
+        temp['name'] = output[i]['name']
 
-    future_contests = output[:idx]
+        temp['startTime'] = cur + timedelta(seconds=-output[i]['relativeTimeSeconds'])
+        temp['endTime'] = temp['startTime'] + timedelta(seconds=output[i]['durationSeconds'])
+        temp['platform'] = 'Codeforces'
+        # print(temp['startTime'],temp['endTime']) 
 
-    future_contests = json.dumps(future_contests)
+        future_contests.append(temp)
+
+    # future_contests = json.dumps(future_contests)
     
     return future_contests
     
